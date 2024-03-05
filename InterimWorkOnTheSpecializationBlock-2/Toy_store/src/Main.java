f1b6f4fdcb195ca7709c7563a93bc3322b330f99
KristiBu <106319724+KristiBu888@users.noreply.github.com>
2024-03-05 11:57:42 +0300

    Add files via upload

---
+++ InterimWorkOnTheSpecializationBlock-2/Toy_store/src/Main.java
@@ -0,0 +1,15 @@
+import java.util.HashMap;
+
+public class Main {
+    public static void main(String[] args) {
+        int countTickets = 15;
+        String fileName = "src/ResultRaffle.txt";
+        Story story = new Story();
+        PrizeToys prizeToys = new PrizeToys(countTickets, story.getListToys());
+        Raffle raffle = new Raffle(countTickets, prizeToys.getToyQueue());
+        HashMap<Integer, String> mapResult = raffle.getPrise();
+        Result result = new Result(mapResult, fileName);
+
+        result.write();
+    }
+}
